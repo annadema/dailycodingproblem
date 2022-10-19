68d641d7eec8ac59de3007980aaa7e370b0df170
annadema <annamaria.demartino@gmail.com>
2022-10-19 14:51:13 +0200

    Create problem1.cpp
    Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
    For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
    Bonus: Can you do this in one pass?

---
+++ problem1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int numbers[] = { 10, 15, 3, 10 };
+	int sum = 20;
+	bool isPresent = false;
+
+	int numbersLength = sizeof(numbers) / sizeof(int);
+	for (int i = 0; i < numbersLength; i++)
+	{
+		for (int j = 0; j < numbersLength; j++)
+		{
+			if (i != j)
+				if ((numbers[i] + numbers[j]) == sum) {
+					isPresent = true;
+					break;
+				}
+		}
+	}
+
+	cout << boolalpha << isPresent << endl;
+	return 0;
+}
